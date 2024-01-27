81a1e9211362fab404a6d9c076ba6de207781080
synodriver <diguohuangjiajinweijun@gmail.com>
2024-01-27 15:31:01 +0800

    bump version

---
+++ changename.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import os
+
+for f in os.listdir("dist"):
+    if "linux" in f:
+        os.rename(
+            os.path.join("dist", f),
+            os.path.join("dist", f.replace("linux", "manylinux2014")),
+        )
